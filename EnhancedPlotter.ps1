8f7a46084875db4e3ea319af854d942f2afa79ec
epidemic0110 <56947388+epidemic0110@users.noreply.github.com>
2021-05-12 00:05:54 -0500

    Update EnhancedPlotter.ps1
    -Added tracking of running plots per tempdrive to enable forward-looking space predictions. Fixes serious bug that would cause a tempdrive to be overfilled and halt all plotting.
    -Fixed a bug that caused the plotting to continue after desired plot count was reached

--- EnhancedPlotter.ps1
+++ EnhancedPlotter.ps1
@@ -20,7 +20,7 @@
     void
 
 .NOTES
-    Version: 1.4.1
+    Version: 1.5
     Author:  /u/epidemic0110
     Email: enhancedchia [@] gmail.com (Send me feedback, please! I'm dyin here!)
     Donation: xch18n2p6ml9sud595kws9m3x38ujh4dgt60sdstk9cpzke9f0qtzrzq079jfg (Hah! Why would anyone would donate their precious Chia XCH?!)
@@ -30,17 +30,17 @@
 #VARIABLES - Set these to match your environment.
 #MANDATORY
 $tempDrives = @("F:","T:") #Drives that you want to use for temp files
-$plotDir = "\\SERVERNAME\PlotFolder" #Local or shared Destination directory you want plots to be sent to (for example, \\SERVERNAME\Plots or G:\Plots)
-$logDir = "C:\temp\EnhancedChiaPlotter"
-$newPlots = 20 #Total number of plots to produce
+$plotDir = "\\SERVERNAME\SharedFolder" #Local or shared Destination directory you want plots to be sent to (for example, \\SERVERNAME\Plots or G:\Plots)
+$newPlots = 30 #Total number of plots to produce
 #OPTIONAL - Advanced settings
+$logDir = "C:\temp\EnhancedChiaPlotter"
 $tempFolder = "\ChiaTemp" #Name of folder to be used/created on the temp drives for temp files
 $temp2Dir = $null #Full path to a directory to be used for staging the finished plot file before it is moved to the final $plotDir destination directory. If set, it uses the -tmp2_dir switch. If $null it does not stage the final files anywhere other than the source temp directory.
-$tempPlotSize = 270 #Size that the temp files for one k32 plot take (Currently ~260GiB/240GB as of v1.1.3)
+$tempPlotSize = 250 #Size that the temp files for one k32 plot take (Currently ~260GiB/240GB as of v1.1.3)
 $threadsPerPlot = 2 #How many processor each plotting process should use. Feel free to experiment with higher numbers on high core systems, but general consesus is that there are diminishing returns above 2 threads
 $delayBetweenChecks = 600 #Delay (in seconds) between checks for sufficient free resources to start a new plot; DON'T SET THIS TOO LOW OR YOU RISK OVER-FILLING A DISK. 300-900 seconds (5-15 minutes) seem to be good values
-$lowDiskThreshold = .60 #A queue length of 1.0 or greater means a disk is saturated and cannot handle any more concurrent requests. Anything less than 1.0 theoretically means the disk isn't fully utilized, however it may not be ideal to target full saturation, especially on mechanical drives. A threshold in the range of .5-.8 is suggested, but tweak and let me know what you find best for overall throughput
-$lowCpuThreshold = 76 #How low should the CPU utilization percent be before a new plot is allowed to be started if other resources are free; This will vary depending on core count and if this is a dedicated plotter or not. If it is doing nothing else and you have lots of cores, you might want to set this to a high value, like 85%. If it is running a full node or farmer, you might want to keep it a bit lower to prevent plotting from using all of the CPU
+$lowDiskThreshold = .70 #A queue length of 1.0 or greater means a disk is saturated and cannot handle any more concurrent requests. Anything less than 1.0 theoretically means the disk isn't fully utilized, however it may not be ideal to target full saturation, especially on mechanical drives. A threshold in the range of .5-.8 is suggested, but tweak and let me know what you find best for overall throughput
+$lowCpuThreshold = 86 #How low should the CPU utilization percent be before a new plot is allowed to be started if other resources are free; This will vary depending on core count and if this is a dedicated plotter or not. If it is doing nothing else and you have lots of cores, you might want to set this to a high value, like 85%. If it is running a full node or farmer, you might want to keep it a bit lower to prevent plotting from using all of the CPU
 $lowMemThreshold = 1024 #Don't start a new plot if it would make system free memory drop below this amount (MB). For example if your $memoryBuffer value is set to 4096MB per plot, and your current system free memory is less than 4096+$lowMemThreshold, it won't start a new plot
 $memoryBuffer = 3390 #Amount of memory to limit each plotting process to. Default is 3390MiB but if you have lots of memory and CPU or disk I/O are your bottleneck, you can try increasing this number
 $initialDelay = 0 #Stagger delay (in minutes) before the very first instance starts on each tempDrive after the first. May help distribute plotting load across phases better, but untested- not sure if helpful or harmful to overall throughput
@@ -53,15 +53,17 @@ if (!(Test-Path $logDir)) { Write-Host "Log directory does not exist, attempting
 if (($temp2Dir -ne $null) -AND !(Test-Path $temp2Dir)) { Write-Host "Temp2 directory does not exist, attempting to create.";New-Item -ItemType Directory -Force -Path $temp2Dir }
 foreach ($tempDrive in $tempDrives) { if (!(Test-Path $tempDrive$tempFolder)) { Write-Host "Temp directory $tempDrive$tempFolder does not exist, attempting to create."; New-Item -ItemType Directory -Force -Path $tempDrive$tempFolder } }
 
+$plots = [hashtable[]]::new($newPlots)
+
 #Main Routine
 cd "~\appdata\local\chia-blockchain\app-*\resources\app.asar.unpacked\daemon"
 
 #Start plotting
-for ($i = 1; $i -le $newPlots){
+for ($i = 0; $i -lt $newPlots){
     #Cycle through temp drives
     foreach ($tempDrive in $tempDrives) {
         #Sleep for initialDelay if this is our first run through each drive
-        if (($i -le $($tempDrives.Count)) -and ($i -gt 1)) {
+        if (($i -lt $($tempDrives.Count)) -and ($i -ge 1)) {
             Write-Host "Sleeping for initial stagger delay of $initialDelay"
             sleep $($initialDelay*60)
         }
@@ -83,21 +85,35 @@ for ($i = 1; $i -le $newPlots){
             if ($CPUAvg -lt $lowCpuThreshold) {
                 Write-Host "Sufficient free CPU of $CPUAvg below threshold of $lowCPUThreshold. Continuing..."
 
+                #Calculate space reserved by existing plot processes
+                $runningPlots = 0
+                $plots | Where-Object {$_.tempDrive -like $tempDrive} | foreach {
+                    if ($(Get-Process -ErrorAction 'SilentlyContinue' -Id $($_.procId)) -ne $null) {
+                        $runningPlots++
+                    }
+                }
+                Write-Host "$runningPlots plots running on $tempDrive"
+                $reservedSpace = $runningPlots * $tempPlotSize*1024*1024*1024
+                Write-Host "$($reservedSpace/1024/1024/1024) GiB reserved for existing plot processes"
                 #Check for sufficient space
                 Write-Host "Checking $tempDrive for sufficient space..."
                 $tempSpace = $(Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID like '$tempDrive'" | select FreeSpace)
-                if ($tempSpace.FreeSpace -gt $($tempPlotSize*1024*1024*1024)){
-                    Write-Host "Free space on $tempDrive $($tempSpace.FreeSpace/1024/1024) GB is at least $tempPlotSize GB. Continuing... "
+                
+                if ($tempSpace.FreeSpace -gt $($tempPlotSize*1024*1024*1024 + $reservedSpace)){
+                    Write-Host "Free space on $tempDrive $([math]::Round($tempSpace.FreeSpace/1024/1024)) GB is large enough to hold new $tempPlotSize GiB plot + $($reservedSpace/1024/1024/1024) GiB running plotting processes. Continuing... "
                     #Check for sufficient disk IO
                     $tempQueue = [math]::Round((Get-Counter "\PhysicalDisk(* $tempDrive)\Avg. Disk Queue Length").CounterSamples.CookedValue,3)
                     if ($tempQueue -lt $lowDiskThreshold) {
                         Write-Host "Disk queue length $tempQueue is below $lowDiskThreshold. Continuing..."
 
                         #Start plot
-                        Write-Host "GO: Spinning off plot $i of $newPlots using $tempDrive in new process" -ForegroundColor Green
+                        Write-Host "GO: Spinning off plot $($i) of $newPlots using $tempDrive in new process" -ForegroundColor Green
                         Write-Host "NEW POWERSHELL WINDOWS WILL CLOSE AUTOMATICALLY WHEN FINISHED. Do not close them unless you want to interrupt them." -ForegroundColor Green
-                        if ($temp2Dir -ne $null) { start-process powershell -ArgumentList ".\chia.exe plots create --size 32 --num 1 --num_threads $threadsPerPlot --tmp_dir `"$tempDrive$tempFolder`" --final_dir `"$plotDir`" --tmp2_dir $temp2Dir --buffer $memoryBuffer -x | Tee-Object -FilePath `"$logDir\Plot$($i)_$(Get-Date -Format dd-mm-yyyy-hh-mm).txt`"" }
-                        else { start-process powershell -ArgumentList ".\chia.exe plots create --size 32 --num 1 --num_threads $threadsPerPlot --tmp_dir `"$tempDrive$tempFolder`" --final_dir `"$plotDir`" --buffer $memoryBuffer -x | Tee-Object -FilePath `"$logDir\Plot$($i)_$(Get-Date -Format dd-mm-yyyy-hh-mm).txt`"" }
+                        $logFile = "Plot$($i)_$(Get-Date -Format dd-mm-yyyy-hh-mm).txt"
+                        if ($temp2Dir -ne $null) { $proc = start-process powershell -ArgumentList ".\chia.exe plots create --size 32 --num 1 --num_threads $threadsPerPlot --tmp_dir `"$tempDrive$tempFolder`" --final_dir `"$plotDir`" --tmp2_dir $temp2Dir --buffer $memoryBuffer -x | Tee-Object -FilePath `"$logDir\$logFile`"" -PassThru }
+                        else { $proc = start-process powershell -ArgumentList ".\chia.exe plots create --size 32 --num 1 --num_threads $threadsPerPlot --tmp_dir `"$tempDrive$tempFolder`" --final_dir `"$plotDir`" --buffer $memoryBuffer -x | Tee-Object -FilePath `"$logDir\$logFile`"" -PassThru }
+                        #Capture plot info
+                        $plots[$i] = @{procId=$proc.Id; tempDrive=$tempDrive; logFile=$logFile; phase=$null}
                         $i++
                         #Quit if we've reached desired plot count, otherwise wait delay and start over
                         if ($i -lt $newPlots) {
@@ -106,12 +122,14 @@ for ($i = 1; $i -le $newPlots){
                         } 
                         else { 
                             Write-Host "Reached desired plot count. Stopping."
-                            break 
+                            Write-Host $plots.count
+                            Read-Host "Press any key to exit"
+                            exit 
                         }
                     }
                     else { Write-Host "$tempDrive queue length is higher than $lowDiskThreshold. Waiting $delayBetweenChecks seconds..."; sleep $delayBetweenChecks }
                 }
-                else { Write-Host "$tempDrive has insufficient space. Only $([math]::Round($tempSpace.FreeSpace/1024/1024))MiB remaining. Waiting $delayBetweenChecks seconds..."; sleep $delayBetweenChecks }
+                else { Write-Host "$tempDrive has insufficient space. Only $([math]::Round($tempSpace.FreeSpace/1024/1024/1024))GiB remaining. Waiting $delayBetweenChecks seconds..."; sleep $delayBetweenChecks }
             }
             else { Write-Host "CPU average of $CPUAvg is not below low CPU threshold of $lowCpuThreshold. Waiting $delayBetweenChecks seconds..."; sleep $delayBetweenChecks }
         }
